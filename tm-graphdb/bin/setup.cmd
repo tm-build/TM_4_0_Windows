c8d2917103e07a3d75f24efa7167f6ca4811ac0d
tm-build <dcruz+tm-build@securityinnovation.com>
2015-05-04 15:58:18 +0000

    adding TM_Design node_modules

--- tm-graphdb/bin/setup.cmd
+++
@@ -1,2 +0,0 @@
-md .\.tmCache
-git clone git@github.com:TMContent/Lib_UNO-json.git .\.tmCache\Lib_UNO-json --quiet
--- tm-graphdb/bin/setup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mkdir ./.tmCache
-git clone git@github.com:TMContent/Lib_UNO-json.git ./.tmCache/Lib_UNO-json
